1c0a53dedc783fe1a8859e2211d5feaa085c6970
Isabel Assuncao <isabelassuncao9@gmail.com>
2023-11-11 16:24:36 -0300

    resolvemos o servidor

--- src/Loaders/mongodb.js
+++ src/Loaders/mongodb.js
@@ -4,6 +4,7 @@ async function startDB() {
   await mogoose.connect(
     "mongodb+srv://mateusbretz:MFyIPDfF2N34YVTc@cursonode.3dtejf3.mongodb.net/?retryWrites=true&w=majority"
   );
+  console.log("db conectado");
 }
 
 module.exports = startDB;
